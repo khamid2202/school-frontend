39aa0437d574022a763a4d34e9c1f6ea460bf7fe
Mamadaliyevamubina <mamadaliyevamubina1234@gmail.com>
2025-09-22 10:37:26 +0500

    dummy data added

--- src/Components/UserPage.jsx
+++ src/Components/UserPage.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { useState, useEffect } from "react";
 
-export default function UserPage() {
+
+useEffect(() => {
+  fetch("/Teachers.json")
+    .then(res => res.json())
+    .then(data => setTeachers(data));
+}, []);
+
+export default function UserPage() 
+{
   return (
     <div>
-      hello, world
+      {teachers.map((teacher) => (
+        <div key={teacher.id}>
+          <h2>{teacher.name}</h2>
+          <p>{teacher.subject}</p>
+          <p>{teacher.email}</p>
+        </div>
+      ))}
     </div>
   )
 }
